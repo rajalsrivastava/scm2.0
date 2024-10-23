ce2207af20eeb86bcbe4434fc46783b83ff756fc
Rajal Srivastava <rajal10srivastava@gmail.com>
2024-10-23 06:52:00 +0530

    Add Bean Validator for user signup form validation

--- scm2.0/src/main/java/com/scm/controllers/PageController.java
+++ scm2.0/src/main/java/com/scm/controllers/PageController.java
@@ -1,14 +1,13 @@
 package com.scm.controllers;
 
-import org.apache.logging.log4j.message.MapMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.scm.entities.User;
 import com.scm.forms.UserForm;
@@ -17,6 +16,7 @@ import com.scm.helpers.MessageType;
 import com.scm.services.UserService;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 
 @Controller
@@ -80,12 +80,16 @@ public class PageController {
     //processing register
 
     @RequestMapping(value="/do-register", method = RequestMethod.POST)
-    public String processRegister(@ModelAttribute UserForm userForm, HttpSession session) {
+    public String processRegister(@Valid @ModelAttribute UserForm userForm, BindingResult rBindingResult, HttpSession session) {
         System.out.println("Processing registration");
         //fetch form data
         //UserForm
         System.out.println(userForm);
         //validate form data
+
+        if (rBindingResult.hasErrors()) {
+            return "register";
+        }
         //todo:Validate userForm[next video]
 
         //save to database
--- scm2.0/src/main/java/com/scm/forms/UserForm.java
+++ scm2.0/src/main/java/com/scm/forms/UserForm.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
 
 @Getter
 @Setter
@@ -13,12 +16,25 @@ import lombok.ToString;
 @NoArgsConstructor
 @Builder
 @ToString
+
 public class UserForm {
 
+    @NotBlank(message = "Username is required")
+    @Size(min=3, message="Min 3 Characters is required")
     private String name;
+
+    @Email(message = "Invalid Email Address")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min=6, message = "Min 6 Characters is required")
     private String password;
+
+    @NotBlank(message="About is required")
     private String about;
+
+    @Size(min=8, max=12, message="Invalid Phone Number")
     private String phoneNumber;
 
 }
