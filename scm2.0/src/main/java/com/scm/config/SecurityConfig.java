cbbea4a887409bdeada17450a5e1131c205cdb64
Rajal Srivastava <rajal10srivastava@gmail.com>
2024-10-27 11:58:01 +0530

    configured the urls

--- scm2.0/src/main/java/com/scm/config/SecurityConfig.java
+++ scm2.0/src/main/java/com/scm/config/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.scm.config;
 
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.scm.services.impl.SecurityCustomUserDetailService;
 
@@ -36,18 +41,36 @@ public class SecurityConfig {
     @Autowired
     private SecurityCustomUserDetailService userDetailService;
 
+    //configuration of authentication provider for spring security
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
 
         //user detail service ka object:
-        daoAuthenticationProvider.setUserDetailsService( userDetailService );
+        daoAuthenticationProvider.setUserDetailsService(userDetailService);
 
         //password encoder ka object
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         return daoAuthenticationProvider;
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        //configuration
+
+        //urls configure kiya hai ki kaun se public rahenge aur kon se private rahenge
+        httpSecurity.authorizeHttpRequests(authorize -> {
+            //authorize.requestMatchers("/home", "/register", "/services").permitAll();
+            authorize.requestMatchers("/user/**").authenticated();
+            authorize.anyRequest().permitAll();
+        });
+
+        //form default login
+        //agar hume kuch bhi change karna hua to hum yahan aayenge : form login se related
+        httpSecurity.formLogin(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
--- scm2.0/src/main/java/com/scm/controllers/PageController.java
+++ scm2.0/src/main/java/com/scm/controllers/PageController.java
@@ -25,6 +25,11 @@ public class PageController {
     @Autowired
     private UserService userService;
 
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/home";
+    }
+
     //sending data to view
     @RequestMapping("/home")
     public String home(Model model) {
